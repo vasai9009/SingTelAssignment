50392af68ca4452db3a8fb7f7b7874e681351a8e
Srinivasa karnam <vasai9009@gmail.com>
2019-11-16 18:52:58 +0530

    Fourth Question Answer

--- src/com/animals/app/TestMain.java
+++ src/com/animals/app/TestMain.java
@@ -1,5 +1,9 @@
 package com.animals.app;
 
+import com.animals.app.characterImpl.Chicken;
+import com.animals.app.characterImpl.Duck;
+import com.animals.app.characterImpl.Parrot;
+
 public class TestMain {
 
 	public static void main(String[] args) {
@@ -8,6 +12,19 @@ public class TestMain {
 		bird.fly();
 		bird.singing();
 		bird.walk();
+		
+		Bird chicken = new Chicken();
+		chicken.singing();
+		Duck duck = new Duck();
+		duck.singing();
+		
+		/* create parrot object according parrots with whom and also need to sound message **/
+		Bird parrotWithDogs = new Parrot("Woof, woof");
+		parrotWithDogs.singing();
+		Bird parrotWithCats = new Parrot("Meow");
+		parrotWithCats.singing();
+		Bird parrotWithRooster = new Parrot("Cock-a-doodle-doo");
+		parrotWithRooster.singing();
 	}
 
 }
